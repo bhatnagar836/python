c0e6e58e9acdbf4f9c06bb3554a3669ee407323c
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-06-16 23:07:12 +0530

    Add files via upload

---
+++ simple_multiplic.py
@@ -0,0 +1,10 @@
+def simple_multiplication(number):
+    print(number * 9) if number % 2 else print(number * 8)
+    return number * 9 if number % 2 else number * 8
+
+
+simple_multiplication(2)
+simple_multiplication(1)
+simple_multiplication(8)
+simple_multiplication(4)
+simple_multiplication(5)
