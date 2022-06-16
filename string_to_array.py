9c736ca5818d05dd1a946ae82265a93cc15bfe0c
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-06-16 23:13:26 +0530

    Add files via upload

---
+++ string_to_array.py
@@ -0,0 +1,14 @@
+def string_to_array(s):
+    print(s.split(" "))
+    return s.split(" ")
+
+
+string_to_array("Robin Singh")
+string_to_array("CodeWars")
+string_to_array("I love arrays they are my favorite")
+string_to_array("1 2 3")
+
+# ["Robin", "Sinh"]
+# ["CodeWars"]
+# ["I", "love", "arrays", "they", "are", "my", "favorite"]
+# ["1", "2", "3"]
