c6879501f9a2ef31a2e20417d28a8829016cef85
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-06-17 22:22:46 +0530

    Add files via upload

---
+++ string_n_times.py
@@ -0,0 +1,10 @@
+def repeat_str(repeat, string):
+    print(repeat * string)
+    return repeat * string
+
+
+repeat_str(3, 'hello ')
+repeat_str(4, 'a')
+repeat_str(2, 'abc')
+repeat_str(6, "I")
+repeat_str(5, "Hello")
