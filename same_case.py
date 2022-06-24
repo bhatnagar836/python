7bf3650df0b8879177c86ac1d4992a4a90aca65f
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-06-24 23:08:45 +0530

    Add files via upload

---
+++ same_case.py
@@ -0,0 +1,8 @@
+def same_case(a, b):
+    if a.isalpha() and b.isalpha():
+        if (a.isupper() and b.isupper()) or (a.islower() and b.islower()):
+            return 1
+        elif (a.isupper() and b.islower()) or (a.islower() and b.isupper()):
+            return 0
+    else:
+        return -1
