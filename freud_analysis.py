58955da03ffcac247db943196817171ae9e65641
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-07-01 22:51:22 +0530

    Add files via upload

---
+++ freud_analysis.py
@@ -0,0 +1,37 @@
+"""You probably know that number 42 is "the answer to life, the universe and everything" according to Douglas Adams'
+"The Hitchhiker's Guide to the Galaxy". For Freud, the answer was quite different...
+
+In the society he lived in, people - women in particular - had to repress their sexual needs and desires. This was
+simply how the society was at the time. Freud then wanted to study the illnesses created by this, and so he digged to
+the root of their desires. This led to some of the most important psychoanalytic theories to this day, Freud being
+the father of psychoanalysis.
+
+Now, basically, when a person hears about Freud, s/he hears "sex" because for Freud, everything was related to, and explained by sex.
+
+In this kata, the function will take a string as its argument, and return a string with every word replaced by the
+explanation to everything, according to Freud. Note that an empty string, or no arguments, should return an empty
+string. """
+
+
+def to_freud(sentence):
+    new_sentence = ""
+    i = 0
+    if len(sentence) == 0:
+        return ""
+    else:
+        words = sentence.split()
+        counter = 0
+        for each_word in words:
+            counter += 1
+        # print(counter)
+
+        for i in range(counter - 1):
+            new_sentence += "sex "
+        # print(new_sentence)
+        new_sentence += "sex"
+        # print(new_sentence)
+        return new_sentence
+
+
+to_freud("This is a test")
+to_freud("You're becoming a true freudian expert")
