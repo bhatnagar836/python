8a6ccd44f447fa9e82b9878effb11c5475793208
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-07-09 22:53:23 +0530

    Add files via upload

---
+++ correct_mistake.py
@@ -0,0 +1,20 @@
+def correct(s):
+    new_s = []
+    for item in s:
+        if item == '0':
+            new_s.append('O')
+        elif item == '1':
+            new_s.append('I')
+        elif item == '5':
+            new_s.append('S')
+        else:
+            new_s.append(item)
+    print(''.join(new_s))
+    return ''.join(new_s)
+
+
+correct("L0ND0N")
+correct("DUBL1N")
+correct("51NGAP0RE")
+correct("BUDAPE5T")
+correct("PAR15")
