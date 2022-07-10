32e05788bd94b5c3192364036c2bf6ed70283791
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-07-10 20:58:17 +0530

    Add files via upload

--- reversed_digits_array.py
+++ reversed_digits_array.py
@@ -9,5 +9,27 @@ def digitize(n):
     return new_list
 
 
+def solution(string):
+    new_s = []
+    for item in string:
+        new_s.append(item)
+        print(new_s)
+    new_s.reverse()
+    print(''.join(new_s))
+    return ''.join(new_s)
+
+# def solution(str):
+#   return str[::-1]
+
+
+solution('world')
+
 digitize(35231)
 digitize(0)
+
+
+
+
+
+
+
