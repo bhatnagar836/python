70eb647ae965b46e3c4053f1197690a2bf0c43b0
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-07-12 15:25:15 +0530

    Add files via upload

---
+++ ascii_total.py
@@ -0,0 +1,16 @@
+def uni_total(s):
+    total = 0
+    if s == "":
+        print(0)
+        return 0
+    else:
+        for items in s:
+            asc_value = ord(items)
+            # print(asc_value)
+            total = total + asc_value
+        print(total)
+        return total
+
+
+uni_total("")
+uni_total("Mary Had A Little Lamb")
