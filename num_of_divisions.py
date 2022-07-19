bf2ccee9deb0e0ab1b79a0f2759a4595e5473e17
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-07-19 16:29:19 +0530

    Add files via upload

---
+++ num_of_divisions.py
@@ -0,0 +1,13 @@
+def divisions(dividend, divisor):
+    counter = 0
+    while dividend >= divisor:
+        quotient = dividend / divisor
+        print(quotient)
+        counter += 1
+        dividend = quotient
+    print(counter)
+    return counter
+
+
+divisions(9999, 3)
+
