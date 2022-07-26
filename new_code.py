20faccd18a927782c2e6a78518c3aae2e5ebddd8
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-07-26 15:59:37 +0530

    Update new_code.py

--- new_code.py
+++ new_code.py
@@ -1,16 +1,19 @@
-def isValid(formula):
-    if 7 or 8 in formula:
-        if (1 and 2 in formula) or (3 and 4 in formula):
-            print(False)
-        else:
-            print(True)
+def is_vow(inp):
+    i = 0
+    while i < len(inp):
+        if chr(inp[i]) == 'a':
+            inp[i] = 'a'
+        elif chr(inp[i]) == 'e':
+            inp[i] = 'e'
+        elif chr(inp[i]) == 'i':
+            inp[i] = 'i'
+        elif chr(inp[i]) == 'o':
+            inp[i] = 'o'
+        elif chr(inp[i]) == 'u':
+            inp[i] = 'u'
+        i += 1
+    print(inp)
+    return inp
 
-        # if 5 and 6:
-        #     # if (1 and !2) or (2 and !1)
-        #     # print(True)
-        #     pass
-    # print(True)
 
-
-isValid([7, 8, 4])
-isValid([5, 6, 7, 8, 2, 4])
+is_vow([101, 121, 110, 113, 113, 103, 121, 121, 101, 107, 103])
