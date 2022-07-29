1dac4dc4e1d39e58046a06ff5eea8fd15028016b
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-07-29 13:38:17 +0530

    Add files via upload

---
+++ sum_of_two_min.py
@@ -0,0 +1,7 @@
+def sum_two_smallest_numbers(numbers):
+    numbers.sort()
+    print(numbers[0]+numbers[1])
+    return numbers[0]+numbers[1]
+
+
+sum_two_smallest_numbers([10, 343445353, 3453445, 3453545353453])
