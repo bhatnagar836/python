4c6c78020f79def05b4173b64dd7c5011067e829
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-08-03 21:39:14 +0530

    Add files via upload

---
+++ new_code3.py
@@ -0,0 +1,17 @@
+def pipe_fix(nums):
+    new_list = []
+    first_ele = nums[0]
+    last_ele = nums[len(nums)-1]
+    print(first_ele, last_ele)
+    while first_ele <= last_ele:
+        new_list.append(first_ele)
+        first_ele += 1
+    print(new_list)
+    return new_list
+
+
+def pipe_fix(nums):
+    return list(range(nums[0], nums[-1] + 1))
+
+
+pipe_fix([6, 9])
