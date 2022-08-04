eeb08b99a5fb9df051c1f6210033e4e64f176ca8
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-08-04 14:37:12 +0530

    Update new_code3.py

--- new_code3.py
+++ new_code3.py
@@ -1,17 +1,12 @@
-def pipe_fix(nums):
-    new_list = []
-    first_ele = nums[0]
-    last_ele = nums[len(nums)-1]
-    print(first_ele, last_ele)
-    while first_ele <= last_ele:
-        new_list.append(first_ele)
-        first_ele += 1
-    print(new_list)
-    return new_list
+def words_to_marks(s):
+    alphabetic_dict = {
+        'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13,
+        'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25,
+        'z': 26}
+    sum_ = 0
+    for items in s:
+        sum_ += alphabetic_dict.get(items)
+    return sum_
 
 
-def pipe_fix(nums):
-    return list(range(nums[0], nums[-1] + 1))
-
-
-pipe_fix([6, 9])
+words_to_marks('friends')
