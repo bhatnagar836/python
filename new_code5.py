e68e95b4a513304480e5f7556a198aea9f22c01a
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-08-13 15:48:59 +0530

    Update new_code5.py

--- new_code5.py
+++ new_code5.py
@@ -1,1 +1,12 @@
-
+def odd_or_even(arr):
+    total = 0
+    if len(arr)==0:
+        return "even"
+    else:
+        for ele in range(len(arr)):
+            total = total + arr[ele]
+            
+    if total % 2:
+        return "odd"
+    else:
+        return "even"
