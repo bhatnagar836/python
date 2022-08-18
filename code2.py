f32042ff8a8f5430c6726be6136c1fcf83f015f5
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-08-18 11:27:18 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,1 +1,8 @@
-
+def divisible_by(numbers, divisor):
+    new_l = []
+    for items in numbers:
+        if items%divisor == 0:
+            new_l.append(items)
+        else:
+            pass
+    return new_l
