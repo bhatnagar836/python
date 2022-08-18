874a09306f6d8d705034b280e5c9f123bcaa0871
bhatnagar836 <62335887+bhatnagar836@users.noreply.github.com>
2022-08-18 11:32:49 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,1 +1,11 @@
-
+def add_length(str_):
+    new_l = []
+    x = str_.split()
+    print(x)
+    for items in x:
+        new_l.append(items + " " + str(len(items)))
+    return new_l
+  
+  
+  add_length('you will win')
+  #["you 3", "will 4", "win 3"]
