39a004972dbbead571a5440a4f7bc24e66f1138f
Pooja Bhatnagar <62335887+bhatnagar836@users.noreply.github.com>
2025-02-20 14:17:05 +0530

    Update tut30.py

--- tut30.py
+++ tut30.py
@@ -1,25 +1,12 @@
-# Excersize 4
-# PATTERN PRINTING
-#  Input : a number and a variable in which we can enter '0' or '1' and typecast 0 to "false" and 1 to "true"
-# If input in true then print the following pattern:
-# *
-# **
-# ***
-# ****
-# Else the following pattern:
-# ****
-# ***
-# **
-# *
+f = open("pooja.txt")
+print(f.tell())  # tells the where the file pointer is pointing currently
+print(f.readline())
+print(f.tell(), "\n")
 
+print("**************************************************************", "\n")
+f.seek((11)) # brings the file pointer to the location specified
+print(f.readline())
+f.seek((0))
+print(f.readline())
 
-n = int(input("Enter how many rows?\n"))
-boolean = int(input("Enter 0 for ascending traingle pattern and 1 for decending traingle pattern\n"))
-print("You have entered ",bool(boolean))
-
-if boolean == 0:
-    for i in range(n+1):
-        print(("*")*i)
-elif boolean == 1:
-    for i in range(n+1):
-        print(("*")* (n-i))
+f.close()
